0d0539c9aaab25a69732634635a7d2c2b4690a8e
Khikhi-16 <minnieee16kp@gmail.com>
2026-02-21 13:12:27 +0530

    Update ProtectedHostRoute.jsx

--- app/web/src/components/ProtectedHostRoute.jsx
+++ app/web/src/components/ProtectedHostRoute.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useHostAuth } from '@/contexts/HostAuthContext.jsx';
 
+const ProtectedHostRoute = ({ children }) => {
+  const { isHostAuthenticated } = useHostAuth();
+  const location = useLocation();
+
+  if (!isHostAuthenticated) {
+    return <Navigate to="/host-login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedHostRoute;
